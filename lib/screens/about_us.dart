52eabbd7f4e53ba84dcdbb22002e58538eac4f87
Abir Ahsan <abirahsan122@gmail.com>
2021-05-09 00:49:00 +0600

    scroll enable for about us

--- lib/screens/about_us.dart
+++ lib/screens/about_us.dart
@@ -9,6 +9,8 @@ class AboutUs extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double _width = MediaQuery.of(context).size.width;
+    double _height = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -19,49 +21,55 @@ class AboutUs extends StatelessWidget {
         ),
         title: Text("About Us"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(
-          top: 25,
-          left: 20,
-          right: 20,
-        ),
-        child: Column(
-          children: [
-            Align(
-              alignment: Alignment.center,
-              child: Text(
-                "Who we are?\n",
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
+      body: Container(
+        width: _width,
+        height: _height,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(
+              top: 25,
+              left: 20,
+              right: 20,
             ),
-            Align(
-              alignment: Alignment.center,
-              child: GestureDetector(
-                onTap: () {
-                  counter++;
-                  if (counter > 10) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      snackBarMessage(
-                        kCredit,
-                        true,
-                      ),
-                    );
-                  }
-                },
-                child: Text(
-                  "We started our journey in 2019 with one goal in mind- to make lives easier. As a technology-first company, we develop tech-driven solutions for the everyday challenges of Bangladeshi people. The DD Super App serves as a single portal connecting local people to local businesses providing a wide range of products and services like food, tickets, rides, truck rentals, and healthcare. Our on-demand service platform provides all the customer care must-haves- a digital marketplace, speedy logistical support, and a customer support call center. DD is more than just a service platform.\n\nDD is a lifestyle. We empower our people by solving and simplifying their daily needs problem and let them enjoy life, the DD way.",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500,
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    "Who we are?\n",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w500,
+                    ),
                   ),
                 ),
-              ),
+                Align(
+                  alignment: Alignment.center,
+                  child: GestureDetector(
+                    onTap: () {
+                      counter++;
+                      if (counter > 10) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          snackBarMessage(
+                            kCredit,
+                            true,
+                          ),
+                        );
+                      }
+                    },
+                    child: Text(
+                      "We started our journey in 2019 with one goal in mind- to make lives easier. As a technology-first company, we develop tech-driven solutions for the everyday challenges of Bangladeshi people. The DD Super App serves as a single portal connecting local people to local businesses providing a wide range of products and services like food, tickets, rides, truck rentals, and healthcare. Our on-demand service platform provides all the customer care must-haves- a digital marketplace, speedy logistical support, and a customer support call center. DD is more than just a service platform.\n\nDD is a lifestyle. We empower our people by solving and simplifying their daily needs problem and let them enjoy life, the DD way.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
