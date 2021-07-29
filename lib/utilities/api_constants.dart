d9297ab1c0aaef9f1450b2af939aeb677284af0a
Alif <ahmedalif1010@gmail.com>
2021-07-29 19:13:23 +0600

    added api_constants

---
+++ lib/utilities/api_constants.dart
@@ -0,0 +1,44 @@
+//API Base URL
+const baseUrl = "https://apps.dd.limited";
+
+//All Category API
+const categoryExt = "/api/v1/category";
+
+//All Vendors API
+const vendorsExt = "/api/v1/vendor/0";
+
+//Category Vendors API
+const categoryVendorsExt = "/api/v1/vendor/where/categoryId/";
+
+//Claim Discount API
+const claimDiscountExt = "/api/v1/customer/claimDiscount";
+
+//Enter OTP & Enter Phone API
+const forgetPasswordExt = "/api/v1/forgotPassword";
+
+const registerExt = "/api/v1/customer/register";
+
+//Guest Login API
+const guestLoginExt = "/api/v1/guest/login";
+
+//Login API
+const loginExt = "/api/v1/login";
+
+//Logout API
+const logoutExt = "/api/v1/logout";
+
+//Renew Subscription API
+const registrationFeeExt = "/api/v1/globals/registrationFee";
+
+const subscriptionFeeExt = "/api/v1/globals/subscriptionFee";
+
+const renewSubscriptionExt = "/api/v1/customer/renewSubscription";
+
+//Top Vendors API
+const topVendorExt = "/api/v1/globals/topVendors";
+
+//User Details Update API
+const userDetailsUpdateExt = "/api/v1/customer/update";
+
+//User Info API
+const customerExt = "/api/v1/customer";
